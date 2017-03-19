2e1ccd7b64ee1093efdd3c5593f3f1a4bf374fff
dobrev <dobrev>
2017-03-19 17:53:21 +0200

    landmarks

--- src/main/java/edu/aubg/virtualtravelparis/LandmarksActivity.java
+++ src/main/java/edu/aubg/virtualtravelparis/LandmarksActivity.java
@@ -2,12 +2,31 @@ package edu.aubg.virtualtravelparis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.content.Context;
+import android.widget.Toast;
 
 public class LandmarksActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landmarks);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(LandmarksActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
