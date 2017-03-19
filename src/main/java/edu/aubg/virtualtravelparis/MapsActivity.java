b8940ca2f07bd15445e3545263781edd32d1a84d
Nikolay Babev <nikolaybabev@gmail.com>
2017-03-19 19:59:16 +0200

    maps to paris

--- src/main/java/edu/aubg/virtualtravelparis/MapsActivity.java
+++ src/main/java/edu/aubg/virtualtravelparis/MapsActivity.java
@@ -39,8 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng paris = new LatLng(48.8566, 2.3522);
+        mMap.addMarker(new MarkerOptions().position(paris).title("Paris"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(8));
     }
 }
