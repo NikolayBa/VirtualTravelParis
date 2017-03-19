639e8ad26e3dca6059869ba66a4552ab92622163
Nikolay Babev <nikolaybabev@gmail.com>
2017-03-19 16:44:07 +0200

    added few activities

--- src/main/java/edu/aubg/virtualtravelparis/MainActivity.java
+++ src/main/java/edu/aubg/virtualtravelparis/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.aubg.virtualtravelparis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void openLandmarks(View view) {
+        Intent intent = new Intent(this, LandmarksActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMaps(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openQuickFacts(View view) {
+        Intent intent = new Intent(this, QuickFactsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ src/main/java/edu/aubg/virtualtravelparis/QuickFactsActivity.java
@@ -0,0 +1,13 @@
+package edu.aubg.virtualtravelparis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuickFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_facts);
+    }
+}
